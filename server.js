475a61ceb96208d6fb1ada3a00ff47c74b9d16d3
Adnan Niaz <adnanniaz77@yahoo.com>
2020-03-13 02:05:09 -0700

    created mongoose connection to DB successfully and applied some colors to beautify

--- server.js
+++ server.js
@@ -4,17 +4,31 @@ const PORT = process.env.PORT || 5000;
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 const color = require("color");
+const chalk = require("chalk");
 
 require("dotenv").config();
+
+// MongoDB connection
+mongoose
+    .connect(process.env.mongoDB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log(chalk.bgCyan.black("Connected to MongoDB"));
+        console.log(chalk.green("**************************************"));
+    })
+    .catch(err => status(200).send(err));
 
 // middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // api route
-app.use("/api", require("./routes/api-routes"));
+app.use("/", require("./routes/api-routes"));
 
 // server listening at port
 app.listen(PORT, () => {
-    console.log("listening at http://localhost:" + PORT);
+    console.log(chalk.green("**************************************"));
+    console.log(chalk.bgCyan.black("listening at http://localhost:" + PORT));
 });
