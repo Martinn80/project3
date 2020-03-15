98ed3a6abbd3cf17f7d1caa3c51e3146b7f201c0
Adnan Niaz <adnanniaz77@yahoo.com>
2020-03-15 15:52:02 -0700

    added header component with some styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,18 @@
 import React, { useState, useEffect } from "react";
 import Search_bar from "./components/Search_bar";
-import Home from "./components/Home";
+import Video from "./components/Video";
 import HeroVideo from "./components/HeroVideo";
+import Header from "./components/Header";
 
 function App() {
     return (
         <div className="App">
             <div className="container">
                 <h1>Youtube App</h1>
+                <Header />
                 <Search_bar />
                 <HeroVideo />
-                <Home />
+                <Video />
             </div>
         </div>
     );
